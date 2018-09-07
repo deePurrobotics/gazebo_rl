f4a6a1fe63e974b25728f0912bca113d80230a38
linzhank <linzhank@gmail.com>
2018-09-07 17:47:12 -0400

    mod task env

--- scripts/openai_ros_envs/crib_task_env.py
+++ scripts/openai_ros_envs/crib_task_env.py
@@ -1,5 +1,5 @@
 import rospy
-import numpy
+import numpy as np
 import time
 from gym import spaces
 import turtlebot_env
@@ -9,163 +9,163 @@ from gym.envs.registration import register
 timestep_limit_per_episode = 10000 # Can be any Value
 
 register(
-        id='TurtleBotCrib-v0',
-        entry_point='openai_ros_envs.crib_task_env:TurtleBotCribEnv',
-        timestep_limit=timestep_limit_per_episode,
-    )
+  id='TurtleBotCrib-v0',
+  entry_point='openai_ros_envs.crib_task_env:TurtleBotCribEnv',
+  timestep_limit=timestep_limit_per_episode,
+)
 
 class TurtleBotCribEnv(turtlebot_env.TurtleBotEnv):
-    def __init__(self):
-        """
-        This Task Env is designed for having the TurtleBot2 in some kind of maze.
-        It will learn how to move around the maze without crashing.
-        """
+  def __init__(self):
+    """
+    This Task Env is designed for having the TurtleBot2 in a walled world.
+    It will learn how to move toward designated point.
+    """
         
-        # Only variable needed to be set here
-        number_actions = rospy.get_param('/turtlebot2/n_actions')
-        self.action_space = spaces.Discrete(number_actions)
+    # Only variable needed to be set here
+    number_actions = rospy.get_param('/turtlebot2/n_actions')
+    self.action_space = spaces.Discrete(number_actions)
         
-        # We set the reward range, which is not compulsory but here we do it.
-        self.reward_range = (-numpy.inf, numpy.inf)
-        
-        
-        #number_observations = rospy.get_param('/turtlebot2/n_observations')
-        """
-        We set the Observation space for the 6 observations
-        cube_observations = [
-            round(current_disk_roll_vel, 0),
-            round(y_distance, 1),
-            round(roll, 1),
-            round(pitch, 1),
-            round(y_linear_speed,1),
-            round(yaw, 1),
-        ]
-        """
-        
-        # Actions and Observations
-        self.linear_forward_speed = rospy.get_param('/turtlebot2/linear_forward_speed')
-        self.linear_turn_speed = rospy.get_param('/turtlebot2/linear_turn_speed')
-        self.angular_speed = rospy.get_param('/turtlebot2/angular_speed')
-        self.init_linear_forward_speed = rospy.get_param('/turtlebot2/init_linear_forward_speed')
-        self.init_linear_turn_speed = rospy.get_param('/turtlebot2/init_linear_turn_speed')
-        
-        self.new_ranges = rospy.get_param('/turtlebot2/new_ranges')
-        self.min_range = rospy.get_param('/turtlebot2/min_range')
-        self.max_laser_value = rospy.get_param('/turtlebot2/max_laser_value')
-        self.min_laser_value = rospy.get_param('/turtlebot2/min_laser_value')
+    # We set the reward range, which is not compulsory but here we do it.
+    self.reward_range = (-np.inf, 0)
         
         
+    #number_observations = rospy.get_param('/turtlebot2/n_observations')
+    """
+    We set the Observation space for the 6 observations
+    cube_observations = [
+      x,
+      y,
+      v_x,
+      v_y,
+      cos(yaw),
+      sin(yaw),
+      w_yaw
+    ]
+    """
         
-        # We create two arrays based on the binary values that will be assigned
-        # In the discretization method.
-        laser_scan = self._check_laser_scan_ready()
-        num_laser_readings = len(laser_scan.ranges)/self.new_ranges
-        high = numpy.full((num_laser_readings), self.max_laser_value)
-        low = numpy.full((num_laser_readings), self.min_laser_value)
+    # Actions and Observations
+    self.linear_speed = rospy.get_param('/turtlebot2/linear_speed')
+    self.angular_speed = rospy.get_param('/turtlebot2/angular_speed')
+    self.init_linear_speed = rospy.get_param('/turtlebot2/init_linear_speed')
+    self.init_angular_speed = rospy.get_param('/turtlebot2/init_angular_speed')
+    self.min_range = rospy.get_param('/turtlebot2/min_range')
+    self.obs_high = rospy.get_param('/turtlebot2/obs_high')
+    self.obs_low = rospy.get_param('/turtlebot2/obs_low')       
+    # We create two arrays based on the binary values that will be assigned
+    # In the discretization method.
+    high = np.array(self.obs_high) # upper bound of observations
+    low = np.array(self.obs_low) # lower bound of observations 
+    # We only use two integers
+    self.observation_space = spaces.Box(low, high)
         
-        # We only use two integers
-        self.observation_space = spaces.Box(low, high)
+    rospy.logdebug("ACTION SPACES TYPE===>"+str(self.action_space))
+    rospy.logdebug("OBSERVATION SPACES TYPE===>"+str(self.observation_space))
         
-        rospy.logdebug("ACTION SPACES TYPE===>"+str(self.action_space))
-        rospy.logdebug("OBSERVATION SPACES TYPE===>"+str(self.observation_space))
-        
-        # Rewards
-        self.forwards_reward = rospy.get_param("/turtlebot2/forwards_reward")
-        self.turn_reward = rospy.get_param("/turtlebot2/turn_reward")
-        self.end_episode_points = rospy.get_param("/turtlebot2/end_episode_points")
+    # Rewards
+    self.reward = rospy.get_param("/turtlebot2/reward")
+    self.end_episode_points = rospy.get_param("/turtlebot2/end_episode_points")
 
-        self.cumulated_steps = 0.0
+    self.cumulated_steps = 0.0
 
-        # Here we will add any init functions prior to starting the MyRobotEnv
-        super(TurtleBotCribEnv, self).__init__()
+    # Here we will add any init functions prior to starting the MyRobotEnv
+    super(TurtleBotCribEnv, self).__init__()
 
-    def _set_init_pose(self):
-        """Sets the Robot in its init pose
-        """
-        self.move_base( self.init_linear_forward_speed,
-                        self.init_linear_turn_speed,
-                        epsilon=0.05,
-                        update_rate=10,
-                        min_laser_distance=-1)
+  def _set_init_pose(self):
+    """Sets the Robot in its init pose
+    """
+    self.move_base(
+      self.init_linear_speed,
+      self.init_angular_speed,
+      epsilon=0.05,
+      update_rate=10,
+      min_laser_distance=-1
+    )
 
-        return True
+    return True
 
 
-    def _init_env_variables(self):
-        """
-        Inits variables needed to be initialised each time we reset at the start
-        of an episode.
-        :return:
-        """
-        # For Info Purposes
-        self.cumulated_reward = 0.0
-        # Set to false Done, because its calculated asyncronously
-        self._episode_done = False
+  def _init_env_variables(self):
+    """
+    Inits variables needed to be initialised each time we reset at the start
+    of an episode.
+    :return:
+    """
+    # For Info Purposes
+    self.cumulated_reward = 0.0
+    # Set to false Done, because its calculated asyncronously
+    self._episode_done = False
         
-        # We wait a small ammount of time to start everything because in very fast resets, laser scan values are sluggish
-        # and sometimes still have values from the prior position that triguered the done.
-        time.sleep(0.2)
+    # We wait a small ammount of time to start everything because in very fast resets, laser scan values are sluggish
+    # and sometimes still have values from the prior position that triguered the done.
+    time.sleep(0.2)
 
 
-    def _set_action(self, action):
-        """
-        This set action will Set the linear and angular speed of the turtlebot2
-        based on the action number given.
-        :param action: The action integer that set s what movement to do next.
-        """
-        
-        rospy.logdebug("Start Set Action ==>"+str(action))
-        # We convert the actions to speed movements to send to the parent class CubeSingleDiskEnv
-        if action == 0: #FORWARD
-            linear_speed = self.linear_forward_speed
-            angular_speed = 0.0
-            self.last_action = "FORWARDS"
-        elif action == 1: #LEFT
-            linear_speed = self.linear_turn_speed
-            angular_speed = self.angular_speed
-            self.last_action = "TURN_LEFT"
-        elif action == 2: #RIGHT
-            linear_speed = self.linear_turn_speed
-            angular_speed = -1*self.angular_speed
-            self.last_action = "TURN_RIGHT"
-        
-        # We tell TurtleBot2 the linear and angular speed to set to execute
-        self.move_base( linear_speed,
-                        angular_speed,
-                        epsilon=0.05,
-                        update_rate=10,
-                        min_laser_distance=self.min_range)
-        
-        rospy.logdebug("END Set Action ==>"+str(action))
+  def _set_action(self, action):
+    """
+    This set action will Set the linear and angular speed of the turtlebot2
+    based on the action number given.
+    :param action: The action integer that set s what movement to do next.
+    """
+    rospy.logdebug("Start Set Action ==>"+str(action))
+    # We convert the actions to speed movements to send to the parent class CubeSingleDiskEnv
+    # if action == 0: #FORWARD
+    #   linear_speed = self.linear_speed
+    #   angular_speed = 0.0
+    #   self.last_action = "FORWARDS"
+    # elif action == 1: #LEFT
+    #   linear_speed = self.linear_turn_speed
+    #   angular_speed = self.angular_speed
+    #   self.last_action = "TURN_LEFT"
+    # elif action == 2: #RIGHT
+    #   linear_speed = self.linear_turn_speed
+    #   angular_speed = -1*self.angular_speed
+    #   self.last_action = "TURN_RIGHT"
 
-    def _get_obs(self):
-        """
-        Here we define what sensor data defines our robots observations
-        To know which Variables we have acces to, we need to read the
-        TurtleBot2Env API DOCS
-        :return:
-        """
-        rospy.logdebug("Start Get Observation ==>")
-        # We get the laser scan data
-        laser_scan = self.get_laser_scan()
+    lin = self.linear_speed # for the sake of create speed pool
+    ang = self.angular_speed
+    lin_spd_pool = [-2*lin, -lin, lin, 2*lin]
+    ang_spd_pool = [-2*ang, -ang, ang, 2*ang]
+    i_lin = action/len(ang_spd_pool)
+    i_ang = action%len(ang_spd_pool) 
+    # We tell TurtleBot2 the linear and angular speed to set to execute
+    self.move_base(
+      linear_speed=lin_spd_pool[i_lin],
+      angular_speed=ang_spd_pool[i_ang],
+      epsilon=0.05,
+      update_rate=10,
+      min_laser_distance=self.min_range
+    )
         
-        discretized_observations = self.discretize_observation( laser_scan,
-                                                                self.new_ranges
-                                                                )
+    rospy.logdebug("END Set Action ==>"+str(action))
 
-        rospy.logdebug("Observations==>"+str(discretized_observations))
-        rospy.logdebug("END Get Observation ==>")
-        return discretized_observations
+  def _get_obs(self):
+    """
+    Here we define what sensor data defines our robots observations
+    To know which Variables we have acces to, we need to read the
+    TurtleBot2Env API DOCS
+    :return:
+    """
+    rospy.logdebug("Start Get Observation ==>")
+    # We get the laser scan data
+    laser_scan = self.get_laser_scan()
+
+    discretized_observations = self.discretize_observation(
+      laser_scan,
+      self.new_ranges
+    )
+
+    rospy.logdebug("Observations==>"+str(discretized_observations))
+    rospy.logdebug("END Get Observation ==>")
+    return discretized_observations
         
 
-    def _is_done(self, observations):
-        
-        if self._episode_done:
-            rospy.logerr("TurtleBot2 is Too Close to wall==>")
-        else:
-            rospy.logerr("TurtleBot2 is Ok ==>")
+  def _is_done(self, observations):
+    if self._episode_done:
+      rospy.logerr("TurtleBot2 is Too Close to wall==>")
+    else:
+      rospy.logerr("TurtleBot2 is Ok ==>")
 
-        return self._episode_done
+    return self._episode_done
 
     def _compute_reward(self, observations, done):
 
@@ -205,9 +205,9 @@ class TurtleBotCribEnv(turtlebot_env.TurtleBotEnv):
         
         for i, item in enumerate(data.ranges):
             if (i%mod==0):
-                if item == float ('Inf') or numpy.isinf(item):
+                if item == float ('Inf') or np.isinf(item):
                     discretized_ranges.append(self.max_laser_value)
-                elif numpy.isnan(item):
+                elif np.isnan(item):
                     discretized_ranges.append(self.min_laser_value)
                 else:
                     discretized_ranges.append(int(item))
