b903bd53e05af9639873560e7dc6b0fa28ed36d7
linzhank <linzhank@gmail.com>
2018-09-09 17:26:23 -0400

    mod task env

--- scripts/gym_test.py
+++ scripts/gym_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import gym
 import numpy
@@ -8,11 +9,27 @@ from gym import wrappers
 # ROS packages required
 import rospy
 import rospkg
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Twist
+from gazebo_msgs.msg import ModelStates, LinkStates
+
 # import our training environment
 from openai_ros_envs import crib_task_env # need write task env
 
 rospy.init_node('gym_test', anonymous=True, log_level=rospy.WARN)    
-env = gym.make('TurtleBotCrib-v0')
+#env = gym.make('TurtleBotCrib-v0')
 
-obs = env.reset()
+#obs = env.reset()
 
+def pose_cb(data):
+  global po
+  global tw
+
+  po = data.pose[-1].position
+  # tw = data.twist[-1]
+  print(po)
+  # print(tw)
+    
+base_pose = rospy.Subscriber("/gazebo/model_states", ModelStates, pose_cb)
+
+rospy.spin()
