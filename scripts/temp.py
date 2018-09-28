03666bacb91e36907a0a1bdbc9fb5c987c52ff60
linzhank <linzhank@gmail.com>
2018-09-28 17:31:58 -0400

    test on some tf datasets

--- scripts/temp.py
+++ scripts/temp.py
@@ -3,18 +3,24 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import tensorflow as tf
 
-r = tf.random_normal([10,3])
-dataset = tf.data.Dataset.from_tensor_slices(r)
-# oneshot = dataset.make_one_shot_iterator()
+# tf.enable_eager_execution()
+
+max_value = tf.placeholder(tf.int64, shape=[])
+dataset = tf.data.Dataset.range(max_value)
 iterator = dataset.make_initializable_iterator()
-# next_shot = oneshot.get_next()
-next_iter = iterator.get_next()
+next_element = iterator.get_next()
 
-sess = tf.Session()
-sess.run(iterator.initializer)
-while True:
-  try:  
-    print(sess.run(r, next_iter))
-  except tf.errors.OutOfRangeError:
-    break
+with tf.Session() as sess:
+  # Initialize an iterator over a dataset with 10 elements.
+  sess.run(iterator.initializer, feed_dict={max_value: 10})
+  for i in range(10):
+    value = sess.run(next_element)
+    print(value)
+    assert i == value
 
+  # Initialize the same iterator over a dataset with 100 elements.
+  sess.run(iterator.initializer, feed_dict={max_value: 100})
+  for i in range(100):
+    value = sess.run(next_element)
+    print(value)
+    assert i == value
