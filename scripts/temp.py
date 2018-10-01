676b42307a58b6cb12ffd91987f713575490f78f
linzhank <linzhank@gmail.com>
2018-10-01 14:59:12 -0400

    test with tf dataset

--- scripts/temp.py
+++ scripts/temp.py
@@ -2,25 +2,46 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 import tensorflow as tf
+import gym
+import rospy
+import random
 
-# tf.enable_eager_execution()
+import openai_ros_envs.crib_task_env
 
-max_value = tf.placeholder(tf.int64, shape=[])
-dataset = tf.data.Dataset.range(max_value)
-iterator = dataset.make_initializable_iterator()
-next_element = iterator.get_next()
+rospy.init_node("dataset_test", anonymous=True, log_level=rospy.DEBUG)
+env_name = "TurtlebotCrib-v0"
+env = gym.make(env_name)
+rospy.loginfo("Gazebo gym environment set")
+# Set parameters
+num_actions = env.action_space.n
+num_states = env.observation_space.shape[0]
+num_episodes = 64
+num_steps = 128
+num_sequences = 100
+horizon = 10 # number of time steps the controller considers
+batch_size = 64
+mem = {"pre_state": [], "action": [], "reward": [], "new_state": []}
 
 with tf.Session() as sess:
-  # Initialize an iterator over a dataset with 10 elements.
-  sess.run(iterator.initializer, feed_dict={max_value: 10})
-  for i in range(10):
-    value = sess.run(next_element)
-    print(value)
-    assert i == value
+  sess.run(tf.global_variables_initializer())
+  # Sample a bunch of random moves
+  rospy.logdebug("Initial random sampling start...")
+  for i in range(2):
+    state, info = env.reset()
+    for j in range(10):
+      if j%100 == 0:
+        rospy.logdebug("Initial random sampling in ep.{}, step{}".format(i+1,j+1))
+      action = random.randrange(num_actions)
+      next_state, reward, done, info = env.step(action)
+      mem["pre_state"].append(state)
+      mem["action"].append(action)
+      mem["reward"].append(reward)
+      mem["new_state"].append(next_state)
+      
+      state = next_state
+    rospy.logdebug("Initial random sampling finished.")
 
-  # Initialize the same iterator over a dataset with 100 elements.
-  sess.run(iterator.initializer, feed_dict={max_value: 100})
-  for i in range(100):
-    value = sess.run(next_element)
-    print(value)
-    assert i == value
+  features = np.concatenate((np.array(mem["pre_state"]), np.array([mem["action"]]).T), axis=1)
+  labels = np.array(mem["new_state"])
+  dataset = tf.data.Dataset.from_tensor_slices((features, labels))
+  
