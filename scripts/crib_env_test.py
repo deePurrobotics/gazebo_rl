95ec9ca52bb61e6aedab1595eb3aee1064f66d64
linzhank <linzhank@gmail.com>
2018-10-05 15:54:06 -0400

    mod crib_env_test to test 100k episodes*128 steps of random actions

--- scripts/crib_env_test.py
+++ scripts/crib_env_test.py
@@ -2,8 +2,9 @@
 from __future__ import print_function
 
 import gym
-import numpy
+import numpy as np
 import time
+import random
 from gym import wrappers
 # ROS packages required
 import rospy
@@ -15,9 +16,23 @@ from gazebo_msgs.msg import ModelStates, LinkStates
 # import our training environment
 from openai_ros_envs import crib_task_env # need write task env
 
-rospy.init_node('env_test', anonymous=True, log_level=rospy.WARN)    
+rospy.init_node('env_test', anonymous=True, log_level=rospy.DEBUG)    
 env = gym.make('TurtlebotCrib-v0')
 
-# obs = env.reset()
 
-# rospy.spin()
+for episode in range(100000):
+  state, info = env.reset()
+  done = False
+  for step in range(128):
+    action = random.randrange(4)
+    next_state, reward, done, info = env.step(action)
+    print("Episode : {}, Step: {}, \nCurrent position: {}, Goal position: {}, Reward: {:.4f}".format(
+      episode,
+      step,
+      info["current_position"],
+      info["goal_position"],
+      reward
+    ))
+    if done:
+      break
+    
