cf53b67cb71fcce5e869694e5a0084e4b887c7ff
deepurrobotics <zhan2838@purdue.edu>
2018-11-26 17:48:01 -0500

    separate tasks in scripts include envs

---
+++ scripts/turtlebot_crib/crib_nav_random_sample.py
@@ -0,0 +1,184 @@
+#! /usr/bin/env python
+
+"""
+Turtlebot random sampling in crib nav task. 
+
+Model input: state-action pairs
+Model output: next state
+
+Author: LinZHanK (linzhank@gmail.com)
+
+Inspired by: https://bair.berkeley.edu/blog/2017/11/30/model-based-rl/
+
+"""
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+import pickle
+import gym
+import rospy
+import random
+import os
+import time
+import datetime
+import matplotlib.pyplot as plt
+
+import envs.crib_nav_task_env
+import utils
+from utils import bcolors
+
+tf.enable_eager_execution()
+
+
+def loss(model, x, y):
+  y_ = model(x)
+  return tf.losses.mean_squared_error(labels=y, predictions=y_)
+
+def grad(model, inputs, targets):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets)
+  return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+def obs_to_state(obs, info):
+  """
+  This function converts observation into state
+  Args: 
+    obs: [x, y, v_x, v_y, cos(yaw), sin(yaw), yaw_dot]
+        theta= robot orientation, alpha= angle between r->g and x-axis
+    info: {"goal_position", ...}
+  Returns:
+    state: [x, y, v_x, v_y, cos(yaw), sin(yaw), yaw_dot, dx, dy, cos(alpha), sin(alpha)]
+  """
+  # create state based on obs
+  state = np.zeros(obs.shape[0]+4)
+  state[:-4] = obs
+  # compute alpha
+  robot_position = obs[:2]
+  goal_position = info["goal_position"]
+  vec_x = np.array([1, 0])
+  vec_y = np.array([0, 1])
+  vec_r2g = goal_position - robot_position
+  cos_alpha = np.dot(vec_r2g, vec_x) / (np.linalg.norm(vec_r2g)*np.linalg.norm(vec_x))
+  sin_alpha = np.dot(vec_r2g, vec_y) / (np.linalg.norm(vec_r2g)*np.linalg.norm(vec_y))
+  # append new states
+  state[-4:-2] = vec_r2g # dx, dy
+  state[-2:] = [cos_alpha, sin_alpha]
+  state = state.astype(np.float32)
+
+  return state
+
+
+if __name__ == "__main__":
+  # init node
+  rospy.init_node("crib_nav_mpc", anonymous=True, log_level=rospy.WARN)
+  # create env
+  env_name = "CribNav-v0"
+  env = gym.make(env_name)
+  rospy.loginfo("Gazebo gym environment set")
+  main_start = time.time()
+  # set parameters
+  num_actions = env.action_space.shape[0]
+  num_states = env.observation_space.shape[0] + 4 # add cos and sin of vector from bot to goal
+  # setup model
+  model = tf.keras.Sequential([
+    tf.keras.layers.Dense(32, activation=tf.nn.relu, input_shape=(num_states+num_actions,)),  # input shape required
+    tf.keras.layers.Dense(32, activation=tf.nn.relu),
+    tf.keras.layers.Dense(num_states)
+  ])
+  stacs_memory = []
+  dstates_memory = []
+  # nextstates_memory = []
+  memory_size = 2**16
+
+  # Random Sampling
+  sample_size = int(memory_size / 20)
+  batch_size = sample_size
+  rs_start = time.time()
+  rospy.logdebug("Start random sampling...")
+  sample_index = 0
+  obs, info = env.reset()
+  done = False
+  state = obs_to_state(obs, info) # to be worked out
+  while sample_index < sample_size:
+    action = env.action_space.sample()
+    obs, _, done, info = env.step(action)
+    next_state = obs_to_state(obs, info) # to be worked out
+    delta_state = next_state - state
+    # append action-state and next state in to the memories
+    stac = np.concatenate((state, action)).astype(np.float32)
+    stacs_memory.append(stac)
+    # nstates_memory.append(next_state) # uncomment this line if you want store next state
+    dstates_memory.append(delta_state)
+    prin(bcolors.OKBLUE, "Sample: {}".format(sample_index+1), bcolors.ENDC)
+    rospy.logdebug(
+      "\ncurrent_state: {}".format(state),
+      "\nnext_state: {}".format(next_state),
+      "\naction: {}".format(action)
+    )
+    sample_index += 1
+    state = next_state
+    if not sample_index % 100:
+      obs, info = env.reset()
+      done = False
+      state = obs_to_state(obs, info) # to be worked out
+  rs_end = time.time()
+  print("Random sampling takes: {:.4f}".format(rs_end-rs_start))
+
+  # Train random sampled dataset
+  num_epochs = 1024
+  dataset = utils.create_dataset(
+    input_features=np.array(stacs_memory),
+    output_labels=np.array(dstates_memory),
+    batch_size=batch_size,
+    num_epochs=num_epochs
+  )
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+  global_step = tf.train.get_or_create_global_step()
+  loss_value, grads = grad(
+    model,
+    np.array(stacs_memory),
+    np.array(dstates_memory)
+  )
+  # create check point
+  model_dir = "/home/linzhank/ros_ws/src/gazebo_rl/scripts/turtlebot/crib_nav/checkpoint"
+  today = datetime.datetime.today().strftime("%Y%m%d")
+  checkpoint_prefix = os.path.join(model_dir, today, "ckpt")
+  if not os.path.exists(os.path.dirname(checkpoint_prefix)):
+    try:
+      os.makedirs(checkpoint_prefix)
+    except OSError as exc: # Guard against race condition
+      if exc.errno != errno.EEXIST:
+        raise
+  root = tf.train.Checkpoint(
+    optimizer=optimizer,
+    model=model,
+    optimizer_step=global_step
+  )
+  root.save(file_prefix=checkpoint_prefix)
+  # start training
+  metrics = tfe.metrics.Mean()
+  rst_start = time.time()
+  for i, (x,y) in enumerate(dataset):
+    # optimize model
+    loss_value, grads = grad(model, x, y)
+    optimizer.apply_gradients(
+      zip(grads, model.variables),
+      global_step
+    )
+    # track progress
+    metrics(loss_value) 
+    # log training
+    if not i % 100:
+      print("Iteration: {}, Loss: {:.3f}".format(i, metrics.result()))
+  rst_end = time.time()
+  print("Random samples training end! It took {:.4f} seconds".format(rst_end-rst_start))
+
+  # save memories
+  memory_dir = "/home/linzhank/ros_ws/src/gazebo_rl/scripts/turtlebot/crib_nav/memories"
+  with open(os.path.join(memory_dir, "stacs_memory.txt"), "wb") as pkfile:
+    pickle.dump(stacs_memory, pkfile)
+  with open(os.path.join(memory_dir, "dstates_memory.txt"), "wb") as pkfile:
+    pickle.dump(dstates_memory, pkfile)
+
---
+++ scripts/turtlebot_playground/playground_envs/playground_fetch_task_env.py
@@ -0,0 +1,214 @@
+from __future__ import print_function
+
+import rospy
+import numpy as np
+import time
+import math
+import random
+import tf
+from gym import spaces
+from .turtlebot_robot_env import TurtlebotRobotEnv
+from gym.envs.registration import register
+
+from gazebo_msgs.msg import ModelState
+from geometry_msgs.msg import Pose, Twist
+
+# Register crib env 
+register(
+  id='PlaygroundFetch-v0',
+  entry_point='envs.playground_fetch_task_env:PlaygroundFetchTaskEnv')
+
+
+class PlaygroundFetchTaskEnv(TurtlebotRobotEnv):
+  def __init__(self):
+    """
+    This task-env is designed for TurtleBot finding a random placed red ball 
+    in its walled playground. Action and state space will be both set to continuous. 
+    """
+    # action limits
+    self.max_linear_speed = .8
+    self.max_angular_speed = math.pi / 3
+    # observation limits
+    self.max_x = 5
+    self.max_y = 5
+    self.max_vx = 2
+    self.max_vy = 2
+    self.max_cosyaw = 1
+    self.max_sinyaw = 1
+    self.max_yawdot = math.pi
+    # action space
+    self.high_action = np.array([self.max_linear_speed, self.max_angular_speed])
+    self.low_action = -self.high_action
+    self.action_space = spaces.Box(low=self.low_action, high=self.high_action)
+    # observation space
+    self.high_observation = np.array(
+      [
+        self.max_x,
+        self.max_y,
+        self.max_vx,
+        self.max_vy,
+        self.max_cosyaw,
+        self.max_sinyaw,
+        self.max_yawdot
+      ]
+    )
+    self.low_observation = -self.high_observation
+    self.observation_space = spaces.Box(low=self.low_observation, high=self.high_observation) 
+    # observation
+    self.observation = np.zeros(self.observation_space.shape[0])
+    self.observation[4] = self.max_cosyaw
+    # info, initial position and goal position
+    self.init_position = np.zeros(2)
+    self.current_position = np.zeros(2)
+    self.previous_position = np.zeros(2)
+    self.goal_position = np.zeros(2)
+    self.info = {}
+    # Set model state
+    self.set_robot_state_publisher = rospy.Publisher("/gazebo/set_model_state", ModelState, queue_size=100)
+    # not done
+    self._episode_done = False
+    # Here we will add any init functions prior to starting the MyRobotEnv
+    super(PlaygroundFetchTaskEnv, self).__init__()
+
+  def _set_init(self):
+    """ 
+    Set initial condition for simulation
+      1. Set turtlebot at a random pose inside crib by publishing /gazebo/set_model_state topic
+      2. Set a goal point inside crib for turtlebot to navigate towards
+    Returns: 
+      init_position: array([x, y]) 
+      goal_position: array([x, y])      
+    """
+    rospy.logdebug("Start initializing robot...")
+    self.current_position = self.init_position
+    # set turtlebot inside crib, away from crib edges
+    mag = random.uniform(0, 1) # robot vector magnitude
+    ang = random.uniform(-math.pi, math.pi) # robot vector orientation
+    x = mag * math.cos(ang)
+    y = mag * math.sin(ang)
+    w = random.uniform(-1.0, 1.0)    
+    robot_state = ModelState()
+    robot_state.model_name = "mobile_base"
+    robot_state.pose.position.x = x
+    robot_state.pose.position.y = y
+    robot_state.pose.position.z = 0
+    robot_state.pose.orientation.x = 0
+    robot_state.pose.orientation.y = 0
+    robot_state.pose.orientation.z = math.sqrt(1 - w**2)
+    robot_state.pose.orientation.w = w
+    robot_state.reference_frame = "world"
+    
+    self.init_position = np.array([x, y])
+    self.previous_position = self.init_position
+
+    # publish model_state to set bot
+    rate = rospy.Rate(100)
+    for _ in range(10):
+      self.set_robot_state_publisher.publish(robot_state)
+      rate.sleep()
+    
+    rospy.logwarn("Robot was initiated as {}".format(self.init_position))
+    # Episode cannot done
+    self._episode_done = False
+    # Give the system a little time to finish initialization
+    rospy.logdebug("Finish initialize robot.")
+    
+    return self.init_position, self.goal_position
+    
+  def _take_action(self, action):
+    """
+    Set linear and angular speed for Turtlebot and execute.
+    Args:
+      action: 2-d numpy array.
+    """
+    rospy.logdebug("TurtleBot2 Base Twist Cmd>>\nlinear: {}\nangular{}".format(action[0], action[1]))
+    cmd_vel = Twist()
+    cmd_vel.linear.x = action[0]
+    cmd_vel.angular.z = action[1]
+    self._check_publishers_connection()
+    rate = rospy.Rate(100)
+    for _ in range(10):
+      self._cmd_vel_pub.publish(cmd_vel)
+      rospy.logdebug("cmd_vel: \nlinear: {}\nangular{}".format(cmd_vel.linear.x,
+                                                               cmd_vel.angular.z))
+      rate.sleep()
+
+  def _get_observation(self):
+    """
+    Get observations from env
+    Return:
+      observation: [x, y, v_x, v_y, cos(yaw), sin(yaw), yaw_dot]
+    """
+    rospy.logdebug("Start Get Observation ==>")
+    model_states = self.get_model_states() # refer to turtlebot_robot_env
+    # update previous position
+    self.previous_position = self.current_position    
+    rospy.logdebug("model_states: {}".format(model_states))
+    x = model_states.pose[-1].position.x # turtlebot was the last model in model_states
+    y = model_states.pose[-1].position.y
+    self.current_position = np.array([x, y])
+    v_x = model_states.twist[-1].linear.x
+    v_y = model_states.twist[-1].linear.y
+    quat = (
+      model_states.pose[-1].orientation.x,
+      model_states.pose[-1].orientation.y,
+      model_states.pose[-1].orientation.z,
+      model_states.pose[-1].orientation.w
+    )
+    euler = tf.transformations.euler_from_quaternion(quat)
+    cos_yaw = math.cos(euler[2])
+    sin_yaw = math.sin(euler[2])
+    yaw_dot = model_states.twist[-1].angular.z
+    
+    self.observation = np.array([x, y, v_x, v_y, cos_yaw, sin_yaw, yaw_dot])
+    rospy.logdebug("Observation ==> {}".format(self.observation))
+    
+    return self.observation
+
+  def _post_information(self):
+    """
+    Return:
+      info: {"init_position", "goal_position", "current_position", "previous_position"}
+    """
+    self.info = {
+      "init_position": self.init_position,
+      "current_position": self.current_position,
+      "previous_position": self.previous_position
+    }
+    
+    return self.info
+
+  def _is_done(self):
+    """
+    If TurtleBot moves into a small circle around the goal, return done==True
+    Args:
+      obs: observation
+      goal: goal position
+    Return:
+      episode_done
+    """
+    
+    return self._episode_done
+
+  def _compute_reward(self):
+    if not self._episode_done:
+    #   if np.linalg.norm(self.current_position-self.goal_position) \
+    #  < np.linalg.norm(self.previous_position-self.goal_position): # if move closer
+    #     reward = 0
+    #   else:
+    #     reward = -1
+      reward = 0
+    else:
+      # if bot reached goal, the init distance will be the reward
+      # reward = np.linalg.norm(self.goal_position-self.init_position)
+      reward = 1
+    rospy.logdebug("Compute reward done. \nreward = {}".format(reward))
+    
+    return reward
+
+
+def _check_publishers_connection(self):
+  raise NotImplementedError()
+
+def _cmd_vel_pub(self):
+  raise NotImplementedError()
